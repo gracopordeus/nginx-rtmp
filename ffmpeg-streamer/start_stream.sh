d8e3b48ed1a4cdf845e24500744e439d3ded903d
Glauco Pordeus <glaucopordeus@outlook.com>
2025-07-11 01:34:55 -0300

    Update start_stream.sh

--- ffmpeg-streamer/start_stream.sh
+++ ffmpeg-streamer/start_stream.sh
@@ -1,3 +1,7 @@
-ffmpeg -i "http://192.168.0.103:8080/video" \
-    -vcodec libx264 -preset ultrafast -tune zerolatency \
-    -f flv rtmp://nginx-rtmp/live/stream
+#!/bin/bash
+
+CAMERA_URL=${CAMERA_URL:-http://host.docker.internal:8080/video}
+
+ffmpeg -i "$CAMERA_URL" \
+  -vcodec libx264 -preset ultrafast -tune zerolatency \
+  -f flv rtmp://nginx-rtmp/live/stream
