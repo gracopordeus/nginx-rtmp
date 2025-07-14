62bd7929ee66629a20d0cd476906a9f605871262
Glauco Pordeus <glaucopordeus@outlook.com>
2025-07-14 20:48:40 -0300

    Delete ffmpeg-streamer directory

--- ffmpeg-streamer/start_stream.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CAMERA_URL=${CAMERA_URL:-http://host.docker.internal:8080/video}
-
-ffmpeg -i "$CAMERA_URL" \
-  -vcodec libx264 -preset ultrafast -tune zerolatency \
-  -f flv rtmp://nginx-rtmp/live/stream
